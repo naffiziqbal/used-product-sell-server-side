42ee6fa94dfb5bd5d0c55a343b1cdb8018013008
naffiziqbal <f.nafiziqbal@gmail.com>
2022-11-23 22:21:06 +0600

    initializing git

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res)=> {
+    res.send("Server Is Online Now")
+})
+
+app.listen(port);
